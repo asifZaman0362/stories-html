ddaadb3ab9872ac8d22d053773ac29f7ba827363
asifZaman0362 <zero362001@gmail.com>
2023-02-02 00:17:47 +0530

    added scrolling and media controls

--- stories-manager.js
+++ stories-manager.js
@@ -17,32 +17,73 @@ class Story {
 
     pages = [];
     element = null;
+    parent = null;
     image_element = null;
     video_element = null;
     page_index = null;
+    buttonPlay = null;
+    buttonMute = null;
+    muted = false;
+    playing = false;
 
     constructor(pages) {
-        let storyWrapper = document.createElement('div');
-        storyWrapper.className = 'story';
+        let storyParent = document.createElement('div');
+        storyParent.className = "story-wrapper";
+
+        let arrow_left = document.createElement('i');
+        arrow_left.classList.add('fa-solid', 'fa-chevron-left');
+        arrow_left.onclick = prevPage;        
+
+        let arrow_right = document.createElement('i');
+        arrow_right.classList.add('fa-solid', 'fa-chevron-right');
+        arrow_right.onclick = nextPage;
+
+        let storyElement = document.createElement('div');
+        storyElement.className = 'story';
+
+        storyParent.appendChild(arrow_left);
+        storyParent.appendChild(storyElement);
+        storyParent.appendChild(arrow_right);
+
         let progressBarContainer = document.createElement('div');
         progressBarContainer.className = "progress-bar-container";
+        
+        let buttonBar = document.createElement('div');
+        buttonBar.className = 'button-bar';
+        this.buttonMute = document.createElement('i');
+        this.buttonMute.classList.add('fa-solid', 'fa-volume-high');
+        this.buttonPlay = document.createElement('i');
+        this.buttonPlay.classList.add('fa-solid', 'fa-play');
+        buttonBar.appendChild(this.buttonMute);
+        buttonBar.appendChild(this.buttonPlay);
+        this.buttonPlay.onclick = togglePlayPause;
+        this.buttonMute.onclick = toggleMute;
+
         let mediaWrapper = document.createElement('div');
         mediaWrapper.className = "page-media-container";
+
         this.video_element = document.createElement('video');
         this.image_element = document.createElement('img');
+
         let cover = document.createElement('div');
         cover.className = "img-cover";
+
         mediaWrapper.appendChild(this.image_element);
         mediaWrapper.appendChild(this.video_element);
         mediaWrapper.appendChild(cover);
-        storyWrapper.appendChild(progressBarContainer);
-        storyWrapper.appendChild(mediaWrapper);
+
+        storyElement.appendChild(progressBarContainer);
+        storyElement.appendChild(buttonBar);
+        storyElement.appendChild(mediaWrapper);
+
         for (var page_data of pages) {
             let page = new StoryPage(page_data);
             this.pages.push(page);
             progressBarContainer.appendChild(page.progressBar);
         }
-        this.element = storyWrapper;
+
+        this.element = storyElement;
+        this.parent = storyParent;
         this.nextPage();
     }
     
@@ -66,7 +107,28 @@ class Story {
             this.image_element.classList.add('hidden');
         }
         page.progressBar.value = 0;
+        this.playing = true;
         return true;
+    }
+
+    prevPage() {
+        if (this.page_index == 0) {
+            return false;
+        } else {
+            this.page_index--;
+            let page = this.pages[this.page_index];
+            if (page.data.type == "image") {
+                this.image_element.classList.remove('hidden');
+                this.image_element.src = page.data.src;
+                this.video_element.classList.add('hidden');
+            } else {
+                this.video_element.classList.remove('hidden');
+                this.video_element.src = page.data.src;
+                this.image_element.classList.add('hidden');
+            }
+            page.progressBar.value = 0;
+            return true;
+        }
     }
     
     tick() {
@@ -78,7 +140,37 @@ class Story {
             return true;
         }
     }
+    
+    toggleMute() {
+        if (this.video_element && this.pages[this.page_index].type == "video") {
+            if (this.muted) {
+                this.video_element.volume = 100;
+                this.buttonMute.classList.replace('fa-volume-off', 'fa-volume-high')
+            }
+            else {
+                this.video_element.volume = 0;
+                this.buttonMute.classList.replace('fa-volume-high', 'fa-volume-off')
+            }
+            this.muted = !this.muted;
+        }
+    }
 
+    togglePlaying() {
+        if (this.playing) {
+            if (this.video_element && this.pages[this.page_index].data.type == "video") {
+                this.video_element.pause();
+            }
+            this.buttonPlay.classList.replace('fa-play', 'fa-pause');
+            pause();
+        } else {
+            if (this.video_element && this.pages[this.page_index].data.type == "video") {
+                this.video_element.play();
+            }
+            this.buttonPlay.classList.replace('fa-pause', 'fa-play');
+            resume();
+        }
+        this.playing = !this.playing;
+    }
 }
 
 let carousel = null;
@@ -99,9 +191,12 @@ function createProgressBar() {
 }
 
 function generateDom() {
+    let background = document.createElement('div');
+    background.className = "transparent-background-carousel";
     carousel = document.createElement('div');
     carousel.className = "stories-carousel";
-    document.body.appendChild(carousel);
+    background.appendChild(carousel);
+    document.body.appendChild(background);
 }
 
 function init() {
@@ -113,9 +208,45 @@ function createStories(stories_data) {
     for (var pages of stories_data) {
         let story = new Story(pages);
         story_elements.push(story);
-        carousel.appendChild(story.element);
+        carousel.appendChild(story.parent);
     }
     start();
+}
+
+function nextPage() {
+    if (focused_story == null) {
+        alert("not running!");
+    } else {
+        if (!story_elements[focused_story].nextPage()) {
+            nextStory();
+        }
+    }
+}
+
+function prevPage() {
+    if (focused_story == null) {
+        alert("not running!");
+    } else {
+        if (!story_elements[focused_story].prevPage()) {
+            prevStory();
+        }
+    }
+    ended = false;
+}
+
+function updateScroll() {
+    if (!started || ended) {
+        return;
+    }
+    if (carousel.clientWidth < document.body.clientWidth) {
+        carousel.style.left = ((document.body.clientWidth - carousel.clientWidth) / 2) + "px";
+    } else {
+        let distanceToFocused = story_elements[focused_story].element.parentElement.offsetLeft;
+        let elementSize = story_elements[focused_story].element.parentElement.clientWidth;
+        let scroll = (-distanceToFocused + ((document.body.clientWidth - elementSize) / 2)) + "px";
+        console.log('scroll: ', distanceToFocused);
+        carousel.style.left = scroll;
+    }
 }
 
 function nextStory() {
@@ -123,6 +254,7 @@ function nextStory() {
         console.log('start');
         focused_story = 0;
         story_elements[focused_story].element.classList.add("focused");
+        story_elements[focused_story].parent.classList.add("focused");
         return;
     }
     else {
@@ -130,9 +262,50 @@ function nextStory() {
             ended = true;
         } else {
             story_elements[focused_story].element.classList.remove("focused");
+            story_elements[focused_story].parent.classList.remove("focused");
             focused_story++;
             story_elements[focused_story].element.classList.add("focused");
+            story_elements[focused_story].parent.classList.add("focused");
         }
+    }
+}
+
+function prevStory() {
+    if (focused_story == 0) {
+        alert("reached beginning!");
+    } else {
+        story_elements[focused_story].element.classList.remove("focused");
+        story_elements[focused_story].parent.classList.remove("focused");
+        focused_story--;
+        let story = story_elements[focused_story];
+        story.element.classList.add("focused");
+        story.parent.classList.add("focused");
+        story.pages[story.pages.length - 1].progressBar.value = 0;
+    }
+    ended = false;
+}
+
+// don't call this manually, only story objects are meant to call this function
+function pause() {
+    clearInterval(timer);
+    timer = null;
+}
+
+// don't call this manually, only story objects are meant to call this function
+function resume() {
+    if (timer == null)
+        restartInterval();
+}
+
+function toggleMute() {
+    if (focused_story != null && focused_story < story_elements.length) {
+        story_elements[focused_story].toggleMute();
+    }
+}
+
+function togglePlayPause() {
+    if (focused_story != null && focused_story < story_elements.length) {
+        story_elements[focused_story].togglePlaying();
     }
 }
 
@@ -142,6 +315,7 @@ function tick() {
             nextStory();
         }
     }
+    updateScroll();
 }
 
 function restartInterval() {
@@ -158,4 +332,20 @@ function start() {
     restartInterval();
 }
 
+function keyListener(event) {
+    switch (event.keyCode) {
+        case 37: {
+            prevPage();
+            break;
+        } case 39: {
+            nextPage();
+            break;
+        } case 32: {
+            togglePlayPause();
+            break;
+        } 
+    }
+}
+
 window.addEventListener('load', init);
+window.addEventListener('keydown', keyListener);
